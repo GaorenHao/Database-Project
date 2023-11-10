5c18be5162c3939248e4739eb6a84f2aee1a9cbf
27ava72 <avalottig@gmail.com>
2023-11-10 21:53:57 +0000

    Ava is not done yet

--- auction/create_auction_result.php
+++ auction/create_auction_result.php
@@ -16,15 +16,32 @@
             make sure it can be inserted into the database. If there is an
             issue, give some semi-helpful feedback to user. */
 
+$sql = "INSERT INTO AuctionItem (ItemAuctionID, SellerID, CategoryID, Description, StartingPrice, ReservePrice, EndDate) VALUES (?, ?, ?, ?, ?, ?, ?) "
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ssssiis", $ItemAuctionID, $SellerID, $CategoryID, $Description, $StartingPrice, $ReservePrice, $EndDate);
+$ItemAuctionID = htmlspecialchars($_POST['Title of auction']);
+$CategoryID = htmlspecialchars($_POST['Category']);
+$Description = htmlspecialchars($_POST['Details']);
+$StartingPrice = htmlspecialchars($_POST['Starting price']);
+$ReservePrice = htmlspecialchars($_POST['Reserve price']);
+$EndDate = htmlspecialchars($_POST['End date']);
+
+
 
 /* TODO #3: If everything looks good, make the appropriate call to insert
             data into the database. */
             
+$stmt->execute ();
 
 // If all is successful, let user know.
 echo('<div class="text-center">Auction successfully created! <a href="FIXME">View your new listing.</a></div>');
 
-
+if ($stmt-> execute()){
+    echo "Auction created successfully!";
+}
+else {
+    echo "Error occured". $stmt->error;
+}
 ?>
 
 </div>
