699aab8b78f997d0db171e1a32214be09257416f
27ava72 <avalottig@gmail.com>
2023-11-15 03:49:21 +0000

    ava_last_update_for_tonight

--- Auction.sql
+++ Auction.sql
@@ -29,7 +29,6 @@ USE Auction;
 
 CREATE TABLE `AuctionItem` (
   `ItemAuctionID` int(4) NOT NULL,
-  'Title' text NOT NULL,
   `SellerID` int(4) NOT NULL,
   `CategoryID` enum('fashion','electronics','beauty','home','outdoor','art') NOT NULL,
   `WatchlistID` int(4) NOT NULL,
