29d6937ea5f52e5244c8cc20284ea5975bdd7978
27ava72 <avalottig@gmail.com>
2023-11-16 13:39:36 +0000

    help

--- mylistings.php
+++ mylistings.php
@@ -8,6 +8,11 @@
 <?php
 
 include 'db_connect.php';
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
   // This page is for showing a user the auction listings they've made.
   // It will be pretty similar to browse.php, except there is no search bar.
   // This can be started after browse.php is working with a database.
@@ -22,33 +27,45 @@ if (isset ($_SESSION['logged_in']) && $_SESSION['logged_in'] == true && $_SESSIO
 
   $UserID = $_SESSION['username'];
 
+  $stmt = $connection -> prepare("SELECT SellerID FROM Sellers WHERE UserID = ?");
+  if($stmt){
 
+    
+    $stmt-> bind_param("i", $UserID);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-  $stmt = $connection -> prepare("SELECT 'SellerID' FROM 'Sellers' WHERE 'UserID' = ?");
-  $stmt-> bind_param("i", $UserID);
-  $stmt->execute();
-  $result = $stmt->get_result();
-
-  if ($result->num_rows>0){
-    $row = $result->fetch_assoc();
-    $SellerID = $row['SellerID']
+    if ($result->num_rows>0){
+      $row = $result->fetch_assoc();
+      $SellerID = $row['SellerID'];
   
 
   
   // TODO: Perform a query to pull up their auctions.
 
-  $stmt = $connection->prepare("SELECT `Title` FROM `AuctionItem` WHERE `SellerID` = ?");
-  $stmt->bind_param("i", $SellerID);
-  $stmt->execute();
-  $result = $stmt->get_result();
+      $stmt = $connection->prepare("SELECT Title, Description, StartingPrice, ReservePrice, EndDate  FROM AuctionItem WHERE SellerID = ?");
+      if($stmt){
+        $stmt->bind_param("i", $SellerID);
+        $stmt->execute();
+        $result = $stmt->get_result();
 
-    if ($result->num_rows>0){
-      while ($row = $result->fetch_assoc()){
-      echo "Item:". $row['Title']. "<br>";
-    }
-    }else{
-      echo "You have no listed items"
-  
+         // TODO: Loop through results and print them out as list items.
+
+        if ($result->num_rows>0){
+          while ($row = $result->fetch_assoc()){
+            $title = $row['Title'];
+            $description = $row['Description'];
+            $startingprice = $row['StartingPrice'];
+            $reserveprice = $row['ReservePrice'];
+            $enddate = $row['EndDate'];
+            print_listing_li($title,$description,$startingprice, $reserveprice, $enddate);
+          }
+        }else{
+          echo "<p>You have no listed items.</p>";
+        }
+    
+      }
+      
     }
   }
 }
@@ -60,7 +77,7 @@ if (isset ($_SESSION['logged_in']) && $_SESSION['logged_in'] == true && $_SESSIO
   
 
 
-  // TODO: Loop through results and print them out as list items.
+ 
   
   
 ?>
