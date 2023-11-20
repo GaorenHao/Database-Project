8d619d81d14e13354810d50d20494d4667ab1544
Gaoren Hao <gaorenh@gmail.com>
2023-11-20 00:31:04 +0000

    edit_acution

---
+++ edit_auction_result.php
@@ -0,0 +1,48 @@
+<?php include_once("header.php")?>
+<div class="container my-5">
+
+<?php
+  include 'db_connect.php';
+
+  // Ensure the user is logged in and is a seller.
+  if (!isset($_SESSION['account_type']) || $_SESSION['account_type'] != 'seller') {
+    echo "You do not have permission to access this page.";
+    exit;
+  }
+
+  // Extract form data
+  $itemAuctionID = $_POST['itemAuctionID'];
+  $auctionTitle = $_POST['auctionTitle'];
+  $auctionDetails = $_POST['auctionDetails'];
+  $auctionStartPrice = $_POST['auctionStartPrice']; // Added this line
+  $auctionReservePrice = $_POST['auctionReservePrice']; // And this line
+  $auctionEndDate = $_POST['auctionEndDate'];
+
+// Convert the auction end date to a DateTime object
+$endDate = DateTime::createFromFormat('Y-m-d\TH:i', $auctionEndDate);
+$now = new DateTime();
+
+// Check if the end date is in the past
+if ($endDate <= $now) {
+    echo "Error: Auction end date must be in the future.";
+    exit;
+}
+if ($auctionReservePrice <= $auctionStartPrice) {
+    echo "Error: Reserve price must be higher than the starting price.";
+    exit;
+  }
+
+  // Update the database
+  $stmt = $connection->prepare("UPDATE AuctionItem SET Title = ?, Description = ?, StartingPrice = ?, ReservePrice = ?, EndDate = ? WHERE ItemAuctionID = ?");
+  $stmt->bind_param("ssddsi", $auctionTitle, $auctionDetails, $auctionStartPrice, $auctionReservePrice, $auctionEndDate, $itemAuctionID);
+
+  if ($stmt->execute()) {
+    echo "Auction item updated successfully.";
+    // Redirect to listings or display success message
+  } else {
+    echo "Error updating item: " . $stmt->error;
+  }
+?>
+
+</div>
+<?php include_once("footer.php")?>
