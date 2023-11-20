86ad1933d5d41a09766909b60bb49ff779dd768c
Gaoren Hao <gaorenh@gmail.com>
2023-11-20 02:53:07 +0000

    2

--- browse.php
+++ browse.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Page Title</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Other head elements -->
+</head>
+<body>
+    <!-- Your HTML content -->
+</body>
+</html>
+
+
 <?php include_once("header.php")?>
 <?php require("utilities.php")?>
 <?php
