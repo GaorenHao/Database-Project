baecd2dae53c512fc26df74032053c5f79c83b3b
Gaoren Hao <gaorenh@gmail.com>
2023-11-20 03:37:14 +0000

    browse_display

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <?php
   // FIXME: At the moment, I've allowed these values to be set manually.
   // But eventually, with a database, these should be set automatically
-  // ONLY after the user's login credentials have been verified via 
+  // ONLY after the user's login credentials have been verified via a
   // database query.
   session_start();
   ini_set('display_errors', 1);
