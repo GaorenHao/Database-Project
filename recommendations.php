825a26b397a131d2ade8376f481e2063dcf2407d
27ava72 <avalottig@gmail.com>
2023-12-01 23:41:26 +0000

    ava

--- recommendations.php
+++ recommendations.php
@@ -2,15 +2,13 @@
 <?php require("utilities.php")?>
 
 <div class="container">
-    <h2 class="my-3">Recommendations for you</h2>
+    <h2 class="my-3">Recommendations</h2>
     <?php
     include 'db_connect.php';
 
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
-
-
 
     if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true && $_SESSION['account_type'] == 'buyer') {
         $UserID = $_SESSION['username'];
@@ -20,7 +18,9 @@
         $stmt->execute();
         $result1 = $stmt->get_result();
 
-        if ($result1->num_rows > 0) {
+        if ($result1->num_rows == 0) {
+            echo "You are not a buyer.";
+        } else {
             $row1 = $result1->fetch_assoc();
             $BuyerID = $row1['BuyerID'];
 
@@ -29,7 +29,7 @@
             $stmt->execute();
             $result2 = $stmt->get_result();
 
-            
+            $recommendations = false; 
 
             while ($row2 = $result2->fetch_assoc()) {
                 $ItemAuctionID = $row2['ItemAuctionID'];
@@ -48,32 +48,32 @@
                     $result4 = $stmt->get_result();
 
                     if ($result4->num_rows > 0) {
-                        $row4 = $result4->fetch_assoc();
-                        $itemAuctionID = $row4["ItemAuctionID"];
+                        $recommendations = true; 
 
-                        $stmt = $connection->prepare("SELECT Title, Description, StartingPrice, EndDate From AuctionItem WHERE ItemAuctionID = ?");
-                        $stmt->bind_param("i", $itemAuctionID);
-                        $stmt->execute();
-                        $result = $stmt->get_result();
+                        while ($row4 = $result4->fetch_assoc()) {
+                            $itemAuctionID = $row4["ItemAuctionID"];
 
-                        while ($row = $result->fetch_assoc()) {
-                        
-                            echo "<li>";
-                            echo "<h3>" . htmlspecialchars($row['Title']) . "</h3>";
-                            echo "<p>Description: " . htmlspecialchars($row['Description']) . "</p>";
-                            echo "<p>Starting Price: " . htmlspecialchars($row['StartingPrice']) . "</p>";
-                            echo "<p>End Date: " . htmlspecialchars($row['EndDate']) . "</p>";
-                            echo "</li>";
+                            $stmt = $connection->prepare("SELECT Title, Description, StartingPrice, EndDate From AuctionItem WHERE ItemAuctionID = ?");
+                            $stmt->bind_param("i", $itemAuctionID);
+                            $stmt->execute();
+                            $result = $stmt->get_result();
+
+                            while ($row = $result->fetch_assoc()) {
+                                echo "<li>";
+                                echo "<h3>" . htmlspecialchars($row['Title']) . "</h3>";
+                                echo "<p>Description: " . htmlspecialchars($row['Description']) . "</p>";
+                                echo "<p>Starting Price: " . htmlspecialchars($row['StartingPrice']) . "</p>";
+                                echo "<p>End Date: " . htmlspecialchars($row['EndDate']) . "</p>";
+                                echo "</li>";
+                            }
+                        }
                     }
-                }else{
-                    echo "You have no recommended items yet. Place a bid and come back to this page.";
                 }
             }
-        }
 
-
-        } else {
-            echo "You are not a buyer.";
+            if (!$recommendations) {
+                echo "You have no recommended items yet. Place a bid and come back to this page.";
+            }
         }
     } else {
         echo "You do not have access.";
